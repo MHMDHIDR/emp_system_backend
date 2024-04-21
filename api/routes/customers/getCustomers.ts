e1a8a231d2a6fbca5d688f1696c4d423a7b76a6a
Mohammed Haydar <mr.hamood277@gmail.com>
2024-04-21 10:39:26 +0100

    Refactor getCustomers function to include admin role check and retrieve clients based on currentEmpId

--- api/routes/customers/getCustomers.ts
+++ api/routes/customers/getCustomers.ts
@@ -14,24 +14,46 @@ import { ITEMS_PER_PAGE } from '../../utils/const'
 export const getCustomers = async (req: any, res: any) => {
   const page = Number(req.params.page) || 1
   const offset = (page - 1) * ITEMS_PER_PAGE
+  const currentEmpId = req.query.currentEmpId
 
   try {
+    // Check if the employee with currentEmpId has the role 'admin'
+    const [adminCheck]: any = await connectDB.query(
+      'SELECT role FROM system_employee_info WHERE employee_id = ? AND role = ?',
+      [currentEmpId, 'admin']
+    )
+
+    let query
+    let queryParams
+    // If the employee has the role 'admin', retrieve all clients
+    if (adminCheck && adminCheck.length > 0) {
+      query = `
+        SELECT id, employee_id, client_name, created_at, nationality,
+               phone_number, email, job_title, office_discovery_method, customer_credentials
+        FROM clients
+        LIMIT ? OFFSET ?
+      `
+      queryParams = [ITEMS_PER_PAGE, offset]
+    } else {
+      // If not 'admin', retrieve clients where client.employee_id === currentEmpId
+      query = `
+        SELECT id, employee_id, client_name, created_at, nationality,
+               phone_number, email, job_title, office_discovery_method, customer_credentials
+        FROM clients
+        WHERE employee_id = ?
+        LIMIT ? OFFSET ?
+      `
+      queryParams = [currentEmpId, ITEMS_PER_PAGE, offset]
+    }
+
     // Get total count of customers
     const [totalCountRows]: any = await connectDB.query(
       'SELECT COUNT(*) as total FROM clients'
     )
     const totalCustomers = totalCountRows[0].total
 
-    // Query to retrieve necessary data from the clients table
-    const query = `
-      SELECT id, employee_id, client_name, created_at, nationality,
-             phone_number, email, job_title, office_discovery_method, customer_credentials
-      FROM clients
-      LIMIT ? OFFSET ?
-    `
-
-    // Get customers' information from the database
-    const [rows]: any = await connectDB.query(query, [ITEMS_PER_PAGE, offset])
+    // Get customers' information from the database based on the determined query
+    const [rows]: any = await connectDB.query(query, queryParams)
 
     // get next page number
     const nextPage = totalCustomers > page * ITEMS_PER_PAGE ? page + 1 : null
