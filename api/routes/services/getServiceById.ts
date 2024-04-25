2f44e867a23cadf40257b9c11b42619260b5a700
Mohammed Haydar <mr.hamood277@gmail.com>
2024-04-25 20:10:58 +0100

    Refactor getServiceById function to handle invalid IDs, improve error handling, and add support for serviceId query parameter

--- api/routes/services/getServiceById.ts
+++ api/routes/services/getServiceById.ts
@@ -12,12 +12,14 @@ import { connectDB } from '../../utils/db'
  */
 export const getServiceById = async (req: any, res: any) => {
   const { id: customerId } = req.params
+  // get serviceId from query params
+  const { serviceId } = req.query
 
   try {
     // Validate customerId is a positive integer
-    if (!customerId && (!/^\d+$/.test(customerId) || parseInt(customerId) < 1)) {
+    if (!customerId && (!/^\d+$/.test(customerId) || parseInt(customerId) < 0)) {
       return res.status(400).json({ error: 'Invalid Service ID' })
-    } else if (customerId && (!/^\d+$/.test(customerId) || parseInt(customerId) < 1)) {
+    } else if (customerId && (!/^\d+$/.test(customerId) || parseInt(customerId) < 0)) {
       return res.status(400).json({ error: 'Invalid Customer ID' })
     }
 
@@ -26,13 +28,23 @@ export const getServiceById = async (req: any, res: any) => {
       ? `SELECT COUNT(*) as total FROM services WHERE id = ?`
       : parseInt(customerId)
       ? `SELECT COUNT(*) as total FROM services WHERE client_id = ?`
+      : parseInt(serviceId) && parseInt(customerId) === 0
+      ? `SELECT COUNT(*) as total FROM services WHERE id = ?`
       : `SELECT COUNT(*) as total FROM services`
     const query = parseInt(customerId)
       ? `SELECT * from services WHERE client_id = ?`
+      : parseInt(serviceId) && parseInt(customerId) === 0
+      ? `SELECT * from services WHERE id = ?`
       : `SELECT * from services`
 
+    console.log('query ->', query)
+
     const [rows]: any = await connectDB.query(query, [
-      parseInt(customerId) ? parseInt(customerId) : parseInt(customerId)
+      parseInt(customerId)
+        ? parseInt(customerId)
+        : parseInt(serviceId)
+        ? parseInt(serviceId)
+        : parseInt(customerId)
     ])
     const [totalCountRows]: any = await connectDB.query(countQuery, [
       parseInt(customerId) ? parseInt(customerId) : parseInt(customerId)
