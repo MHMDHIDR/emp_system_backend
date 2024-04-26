26417d6c3c780ba760aa96840ab3d0c35458f943
Mohammed Haydar <mr.hamood277@gmail.com>
2024-04-26 14:20:56 +0100

    Refactor getServices function to include employeeId query parameter and improve query construction

--- api/routes/services/getServices.ts
+++ api/routes/services/getServices.ts
@@ -10,20 +10,25 @@ import { ITEMS_PER_PAGE } from '../../utils/const'
  */
 export const getServices = async (req: any, res: any) => {
   const page = req.params.page || 1
+  const employeeId = req.query.employeeId
   const offset = (page - 1) * ITEMS_PER_PAGE
 
+  // construct the query to get the services and the total count of services
+  const query = employeeId
+    ? `SELECT * FROM services WHERE employee_id = ? LIMIT ? OFFSET ?`
+    : `SELECT * FROM services LIMIT ? OFFSET ?`
+
+  const getTotalQuery = employeeId
+    ? `SELECT COUNT(*) as total FROM services WHERE employee_id = ?`
+    : `SELECT COUNT(*) as total FROM services`
+
   try {
-    // Get total count of customers
-    const [totalCountRows]: any = await connectDB.query(
-      'SELECT COUNT(*) as total FROM services'
-    )
+    // Get total count of services
+    const [totalCountRows]: any = await connectDB.query(getTotalQuery, [employeeId])
     const totalServices = totalCountRows[0].total
 
-    // Query to retrieve necessary data from the services table
-    const query = `SELECT * FROM services LIMIT ? OFFSET ?`
-
-    // Get customers' information from the database
-    const [rows]: any = await connectDB.query(query, [ITEMS_PER_PAGE, offset])
+    // Get services' information from the database
+    const [rows]: any = await connectDB.query(query, [employeeId, ITEMS_PER_PAGE, offset])
 
     // Send the fetched data as a response along with total count
     res.status(200).json({ rows, totalServices })
